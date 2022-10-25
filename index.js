c494b8a47f89ebf54eebad5e0f00b00c1d7d20e2
kebin20 <ktanzyl@hotmail.com>
2022-10-25 19:39:12 +0900

    Completed the watchlist of JS, added assets

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const searchBox = document.getElementById("search-box");
 const searchButton = document.getElementById("search-button");
 const populatedMovies = document.getElementById("populated-movies");
-const populatedWatchlist = document.getElementById(
-  "populated-movies-watchlist"
-);
 const noResultScreen = document.getElementById("no-result");
 
 let addedMovies = [];
@@ -20,7 +17,6 @@ let movie = {
       return this.getMoviesHtml(data);
     } catch {
       noResultScreen.style.display = "block";
-      // alert("No movie found.");
     }
   },
 
@@ -43,14 +39,13 @@ let movie = {
         <div class="row-2">
             <p class="movie-runtime">${Runtime}</p>
             <p class="movie-genre">${Genre}</p>
-            <button id="add-to-watchlist-btn" data-movie="${imdbID}">ADD</button><p class="button-content">Watchlist</p>
+            <button id="add-to-watchlist-btn" data-movie="${imdbID}"></button><p class="button-content">Watchlist</p>
         </div>
         <p class="movie-content">${Plot}</p>
         </div>
     </div>
   `;
       movieArrayData.push(data);
-      console.log(movieArrayData); //check; its working!
     }
     populatedMovies.innerHTML = moviesHtml;
   },
@@ -74,15 +69,10 @@ searchBox.addEventListener("keyup", (e) => {
 
 //Adding to the watchlist function
 
-// TESTING LOCALSTORAGE
-// localStorage.setItem("addedMovies", JSON.stringify(addedMovies));
-// let moviesFromLocalStorage = JSON.parse(localStorage.getItem("addedMovies"));
-// console.log(moviesFromLocalStorage);
-
 document.addEventListener("click", (e) => {
   if (e.target.dataset.movie) {
-    console.log(e.target.dataset.movie); //check; its working!
     addMovies(e.target.dataset.movie);
+    alert("Movie added to watchlist!");
   } else if (e.target.dataset.indexNumber) {
     removeMovie(e.target.dataset.indexNumber);
   }
@@ -93,67 +83,5 @@ function addMovies(movieId) {
     return movie.imdbID == movieId;
   })[0];
   addedMovies.push(targetMovie);
-  console.log(addedMovies); //check; its working!
-  //Problem? Can't stringify an object.... check how to use localsStorage properly since this flow might work without localStorage
-  // localStorage.setItem("addedMovies", JSON.stringify(addedMovies)); //check; not working :(
-  // let moviesFromLocalStorage = JSON.parse(localStorage.getItem("addedMovies"));
-  // renderWatchlist(moviesFromLocalStorage);
+  localStorage.setItem("addedMovies", JSON.stringify(addedMovies));
 }
-
-function removeMovie(index) {
-  addedMovies.splice(index, 1);
-  renderWatchlist();
-}
-
-function renderWatchlist(moviesFromLocalStorage) {
-  const watchlistHtml = moviesFromLocalStorage.map((watchlist, index) => {
-    return `
-        <div class="movie-container">
-        <img src="${watchlist.Poster}" alt="Image of movie">
-        <div class="info-row">
-        <div class="row-1">
-            <h3 class="movie-title">${watchlist.Title}</h3>
-            <i class="star-icon">⭐</i> <span class="rating-value">${watchlist.imdbRating}</span>
-        </div>
-        <div class="row-2">
-            <p class="movie-runtime">${watchlist.Runtime}</p>
-            <p class="movie-genre">${watchlist.Genre}</p>
-            <a href="#" id="remove-from-watchlist-btn" data-index-number="${index}"><i class="fa-solid fa-circle-plus fa-lg"></i></a><p class="button-content">REMOVE PLS</p>
-        </div>
-        <p class="movie-content">${watchlist.Plot}</p>
-        </div>
-    </div>
-    `;
-  });
-  document.getElementById("populated-movies-watchlist").innerHTML =
-    watchlistHtml;
-}
-
-// /* <i class="fa-solid fa-circle-plus fa-lg"></i> */
-
-// return this.displayMovies(data)
-//THIS IS TO DISPLAY ONE MOVIE ONLY
-//   displayMovies: function (data) {
-//     const poster = data.Poster;
-//     const title = data.Title;
-//     const rating = data.imdbRating;
-//     const runTime = data.Runtime;
-//     const genre = data.Genre;
-//     const plot = data.Plot;
-
-//     populatedMovies.innerHTML = `
-//         <img src="${poster}" alt="Image of movie">
-//         <div class="info-row">
-//         <div class="row-1">
-//             <h3 class="movie-title">${title}</h3>
-//             <i class="star-icon">⭐</i> <span class="rating-value">${rating}</span>
-//         </div>
-//         <div class="row-2">
-//             <p class="movie-runtime">${runTime}</p>
-//             <p class="movie-genre">${genre}</p>
-//             <a href="#" id="add-to-watchlist-btn"><i class="fa-solid fa-circle-plus fa-lg"></i></a><p class="button-content">Add to Watchlist</p>
-//         </div>
-//         <p class="movie-content">${plot}</p>
-//         </div>
-//     `;
-//   },
---
+++ watchlist.js
@@ -0,0 +1,46 @@
+let moviesFromLocalStorage = JSON.parse(localStorage.getItem("addedMovies"));
+// console.log(moviesFromLocalStorage); //check; its working!
+localStorage.clear();
+
+function renderWatchlist(moviesFromLocalStorage) {
+  const watchlistHtml = moviesFromLocalStorage.map((watchlist, index) => {
+    console.log(watchlist);
+    console.log(index);
+    return `
+          <div class="movie-container">
+          <img src="${watchlist.Poster}" alt="Image of movie">
+          <div class="info-row">
+          <div class="row-1">
+              <h3 class="movie-title">${watchlist.Title}</h3>
+              <i class="star-icon">⭐</i> <span class="rating-value">${watchlist.imdbRating}</span>
+          </div>
+          <div class="row-2">
+              <p class="movie-runtime">${watchlist.Runtime}</p>
+              <p class="movie-genre">${watchlist.Genre}</p>
+              <button id="remove-from-watchlist-btn" data-index-number="${index}"></button><p class="button-content">Remove</p>
+          </div>
+          <p class="movie-content">${watchlist.Plot}</p>
+          </div>
+      </div>
+      `;
+  });
+
+  if (document.getElementById("populated-movies-watchlist") != null) {
+    document.getElementById("populated-movies-watchlist").innerHTML +=
+      watchlistHtml.join("");
+  }
+  document.querySelector(".empty-watchlist").style.display = "none";
+}
+
+document.addEventListener("click", (e) => {
+  if (e.target.dataset.indexNumber) {
+    removeMovie(e.target.dataset.indexNumber);
+  }
+});
+
+function removeMovie(index) {
+  moviesFromLocalStorage.splice(index, 1);
+  renderWatchlist();
+}
+
+renderWatchlist(moviesFromLocalStorage);
